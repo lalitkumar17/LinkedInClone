dad4acebcfe8acac05bd8292a947e619bab0be12
lalitkumar17 <lalitkumar1736069@gmail.com>
2022-04-15 22:02:57 +0530

    refractor code and added search box in navigation

--- linked-in-clone/src/components/NavBar/NavBar.js
+++ linked-in-clone/src/components/NavBar/NavBar.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./NavBar.scss";
 import { images } from "../../constent/images";
+import SearchIcon from "@mui/icons-material/Search";
 
 const NavBar = () => {
   return (
     <div className="navbar">
       <img src={images.linkedin} alt="" className="navbar__image" />
+      <div className="navbar__search-box">
+        <SearchIcon className="searchbox-icon" />
+        <input type="text" />
+      </div>
     </div>
   );
 };
